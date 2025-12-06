f53962a611cac21a7f7b17d179ccae46cadd998f
Benim-Ahmed <mohiuddinahmad18@gmail.com>
2025-12-07 00:52:53 +0500

    Added Show all products functionality

--- user.cpp
+++ user.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <cctype>
+#include <iomanip>
 using namespace std;
 
 void loginPanel();
@@ -10,10 +11,20 @@ void createAccount();
 bool checkPasswordAuth(string password);
 void userPanel();
 bool savingUserAccount(string username, string password);
+void displayAllProducts();
 
 struct Accounts {
     string username;
     string password;
+};
+
+struct Products {
+    int id;
+    string name;
+    double price;
+    string brand;
+    string category;
+    int quantity;
 };
 
 int main() {
@@ -164,7 +175,8 @@ void userPanel() {
     cout << "-------------------\n" << endl;
     cout << "1. Show All Products" << endl;
     cout << "2. Search Product" << endl;
-    cout << "3. Logout" << endl;
+    cout << "3. Go Back" << endl;
+    cout << "4. Logout" << endl;
 
     while(true) {
         cout << "Enter choice(1-3): ";
@@ -175,9 +187,32 @@ void userPanel() {
             cout << "Wrong Choice! Try Again" << endl;
         }
     }
-    // pending
-    // pending
-    // pending
+    if (choice == 1){
+        cout << "Following products are available in the store:" << endl;
+        displayAllProducts();
+    }
+    else if (choice == 2){
+        //pending
+        //pending
+        //pending
+    }
+    else if (choice == 3){
+        userPanel();
+    }
+    else if (choice == 4){
+        loginPanel();
+    }
+}
+
+void displayAllProducts(){
+    Products products;
+    ifstream Products("products.txt");
+    string line;
+    cout << "id" << setw(20) << "Product Name" << setw(20) << "Price" << setw(20) << "brand" << setw(20) << "Category" << setw(20) << "Quantity" << endl;
+    cout << "--------------------------------------------------------------------------------------------------------------" << endl;
+    while (Products >> products.id >> products.name >> products.price >> products.brand >> products.category >> products.quantity){
+        cout << products.id << setw(20) << products.name << setw(20) << products.price << setw(20) << products.brand << setw(20) << products.category << setw(20) << products.quantity << endl;
+    }
 }
 
 bool savingUserAccount(string username, string password) {
